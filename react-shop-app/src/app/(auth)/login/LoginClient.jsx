14d64280c341f7509fb9021df0a825db4ddbf9c8
Lee Ye Seo <rosefor2ne@gmail.com>
2024-06-12 14:02:09 +0900

    design: Loader 컴포넌트 생성 및 로그인 페이지 적용

--- react-shop-app/src/app/(auth)/login/LoginClient.jsx
+++ react-shop-app/src/app/(auth)/login/LoginClient.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import LogoPath from '@/assets/colorful.svg';
 import { useRouter } from 'next/navigation';
 import styles from './Auth.module.scss';
+import Loader from '@/components/loader/Loader';
 
 const LoginClient = () => {
   const [email, setEmail] = useState('');
@@ -25,25 +26,32 @@ const LoginClient = () => {
   }
 
   return (
-    <section className={styles.page}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>
-          <Image priority src={LogoPath} alt='logo' />
-        </h1>
-        <form onSubmit={loginUser} className={styles.form}>
-          {/* input */}
-          <div className={styles.group}>
-            {/* 자동 로구인, 비밀반호 수정 */}
-          </div>
-          <div className={styles.buttonGroup}>
-            {/* Button */}
-            <div>
-              {/* Button */}
+    <>
+      <Loader/>
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image priority src={LogoPath} alt='logo' />
+          </h1>
+          <form onSubmit={loginUser} className={styles.form}>
+            {/* input */}
+            input
+            <div className={styles.group}>
+              {/* 자동 로구인, 비밀반호 수정 */}
+              자동 로구인, 비밀반호 수정
             </div>
-          </div>
-        </form>
-      </div>
-    </section>
+            <div className={styles.buttonGroup}>
+              {/* Button */}
+              Button
+              <div>
+                {/* Button */}
+                Button
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   )
 }
 

649219c041ecbb73eef42196c886bfebc6b9e054
Lee Ye Seo <rosefor2ne@gmail.com>
2024-06-12 13:56:41 +0900

    design: login UI 생성하기

---
+++ react-shop-app/src/app/(auth)/login/LoginClient.jsx
@@ -0,0 +1,50 @@
+'use client'
+import Image from 'next/image';
+import React, { useState } from 'react';
+import LogoPath from '@/assets/colorful.svg';
+import { useRouter } from 'next/navigation';
+import styles from './Auth.module.scss';
+
+const LoginClient = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAutoLogin, setIsAutoLogin] = useState(false);
+
+  const router = useRouter();
+  const redirectionUser = () => router.push('/');
+
+  const loginUser = (e) => {
+    //인증은 firebase 사용할 것임
+    e.preventDefault();
+    setIsLoading(true);
+  }
+
+  const signInWithGoogle = () => {
+    //인증은 firebase 사용할 것임
+  }
+
+  return (
+    <section className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
+          <Image priority src={LogoPath} alt='logo' />
+        </h1>
+        <form onSubmit={loginUser} className={styles.form}>
+          {/* input */}
+          <div className={styles.group}>
+            {/* 자동 로구인, 비밀반호 수정 */}
+          </div>
+          <div className={styles.buttonGroup}>
+            {/* Button */}
+            <div>
+              {/* Button */}
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default LoginClient
